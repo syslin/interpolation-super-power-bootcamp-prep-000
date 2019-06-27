29c33e63d1e99a7bafc224f7586009e1933a12d6
Sanima Shrestha <saneemastha@gmail.com>
2019-06-27 05:57:29 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,6 +3,12 @@ def display_rainbow(color)
  
  # color. each |x|
   puts "R:#{color[0]}"
+  puts "R:#{color[1]}"
+  puts "R:#{color[2]}"
+  puts "R:#{color[3]}"
+  puts "R:#{color[4]}"
+  puts "R:#{color[5]}"
+  puts "R:#{color[6]}"
   
 end
  color=["red","orange","yellow","green","blue","indigo","voilet"]
